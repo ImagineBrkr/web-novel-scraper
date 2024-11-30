0757b4a533d26207097e018c070893aa7e29003f
ImagineBrkr <salvattore_25@hotmail.com>
2024-11-30 13:53:55 -0500

    First commit

---
+++ custom_logger.py
@@ -0,0 +1,52 @@
+import logging
+
+process = "main"
+
+class CustomFormatter(logging.Formatter):
+
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    green = "\x1b[32;20m"
+    reset = "\x1b[0m"
+    format_str = f"%(asctime)s - %(name)s - %(levelname)s {reset}- %(operation)s - %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: grey + format_str + reset,
+        logging.INFO: green + format_str + reset,
+        logging.WARNING: yellow + format_str + reset,
+        logging.ERROR: red + format_str + reset,
+        logging.CRITICAL: bold_red + format_str + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+def create_logger(operation):
+    logger = logging.getLogger(process)
+    logger.setLevel(logging.DEBUG)
+
+    if not logger.handlers:
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)  # Set this to the minimum logging level that you want
+
+        # Create formatter
+        formatter = CustomFormatter()
+
+        # Add formatter to ch
+        ch.setFormatter(formatter)
+
+        # Add ch to logger
+        logger.addHandler(ch)
+
+    extra = {'operation': operation}
+    logger = logging.LoggerAdapter(logger, extra)
+
+    return logger
+
+def set_process(new_process):
+    global process
+    process = new_process
