28dcd36b01615f7f374992406b310ffa7003d25f
EEduardoSanandres <102604575+EEduardoSanandres@users.noreply.github.com>
2025-01-25 18:30:22 -0500

    fix scrap_chapter error logs

--- web_novel_scraper/__main__.py
+++ web_novel_scraper/__main__.py
@@ -332,17 +332,21 @@ def scrap_chapter(title, novel_base_dir, chapter_url, chapter_num, update_html):
     """Scrap a chapter of a novel."""
     novel = obtain_novel(title, novel_base_dir)
     if not chapter_url and not chapter_num:
-        click.echo('Chapter URL or chapter number should be set.', err=True)
-    if chapter_num and chapter_url:
-        click.echo('It should be either chapter URL or chapter number.', err=True)
-    if chapter_num <= 0 or chapter_num > len(novel.chapters):
-        raise click.BadParameter(
-            'Chapter number should be positive and an existing chapter.', param_hint='--chapter-num')
+        raise click.ClickException('Chapter URL or chapter number should be set.')
+    if chapter_url and chapter_num:
+        raise click.ClickException('It should be either chapter URL or chapter number.')
+    if chapter_num is not None:
+        if chapter_num <= 0 or chapter_num > len(novel.chapters):
+            raise click.ClickException(
+                'Chapter number should be positive and within existing chapters.'
+            )
     chapter = novel.scrap_chapter(
-        chapter_url=chapter_url, chapter_idx=chapter_num - 1, update_html=update_html)
+        chapter_url=chapter_url,
+        chapter_idx=(chapter_num - 1) if chapter_num else None,
+        update_html=update_html
+    )
     if not chapter:
-        click.echo('Chapter number or URL not found.', err=True)
-        return
+        raise click.ClickException('Chapter number or URL not found.')
     click.echo(chapter)
     click.echo('Content:')
     click.echo(chapter.chapter_content)
