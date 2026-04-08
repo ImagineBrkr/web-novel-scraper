6ad566ad302d7183ce28094e7c353541aaafbb17
ImagineBrkr <salvattore_25@hotmail.com>
2026-04-07 20:54:30 -0500

    Fix Syntax

--- web_novel_scraper/custom_processor/__init__.py
+++ web_novel_scraper/custom_processor/__init__.py
@@ -1,6 +1,2 @@
 from .custom_processor import CustomProcessor, ProcessorRegistry
-<<<<<<< HEAD
-from .sites import royalroad, genesis, fanmtl, novelhi, novelbin, mtlnovels, novelingua
-=======
-from .sites import royalroad, genesis, fanmtl, novelhi, novelbin, mtlnovels, empirenovel
->>>>>>> 00f18d8 ([HOST/015]: empirenovel.com host)
+from .sites import royalroad, genesis, fanmtl, novelhi, novelbin, mtlnovels, novelingua, empirenovel
